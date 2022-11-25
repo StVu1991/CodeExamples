6d6488d9f4b9ca595bf053e71e950f56f59656e4
svukelic <vukelic.stjepan@gmail.com>
2022-11-25 13:34:00 +0100

    Update pg_select_from.sql

--- pg_select_from.sql
+++ pg_select_from.sql
@@ -1,5 +1,13 @@
+====================================
+name	   country	population	  area
+Tokyo	    Japan	   38505000	    8223
+Shangai 	China	   22125000	    4015
+====================================
 # Daj mi sve kolone
 SELECT * FROM cities;
 
 # Daj mi određene kolone
 SELECT name, population FROM cities;
+
+# Daj mi imaginarnu kolonu kao rezultat matematičke operacije 2 postojeće kolone
+SELECT name, population/area as density FROM cities;
